d138acadc8aa82107fe8884ba110d194af80d770
angel-20-12 <angel.lardi@gmail.com>
2020-01-04 18:08:56 +0200

    kak smogla

---
+++ app.js
@@ -0,0 +1,31 @@
+$ (document).ready(function(){
+     $('.news').slick(
+		{
+			// speed: 1300,
+            autoplay: false,
+            arrows: true,
+			dots: true,
+			slidesToShow: 3,
+            slidesToScroll: 5,
+            // vertical: true,
+            // infinity: false,
+			// fade: true,
+            centerMode: true
+            // appendDots: $('.ba-banner'),
+			// variableWidth: true,
+            // responsive: [
+            //     {
+            //         breakpoint: 1024,
+            //         settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 3,
+            //         infinite: true,
+            //         dots: true
+            //         }
+            //     },
+            // ]
+
+
+		})
+
+})
